4c3dc14d3bebd12e79b823adce438ddfa2697147
Suguru-Ueno <73816149+Suguru-Ueno@users.noreply.github.com>
2020-11-02 17:19:41 +0900

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+'use strict'
+
+$(function(){
+  $('.toggle_btn').on('click',function(){
+    $('#page-header').toggleClass('open')
+  });
+});
+
+$(function(){
+  $('#mask').on('click',function(){
+    $('#page-header').removeClass('open')
+  });
+});
