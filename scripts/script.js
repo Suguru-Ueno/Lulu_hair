19dfbf54e46db9f8fd1700e18a88ed8f58c2e400
Suguru_Ueno <sgr_ueno@outlook.com>
2020-11-19 17:35:41 +0900

    画像ファイルの名前を統一した。 javascriptでmainimgをスライドショーにした(妥協)

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,27 @@
 'use strict'
 
+const images = ['images/Lulu1.jpg','images/Lulu2.jpg','images/Lulu3.jpg','images/Lulu4.jpg','images/Lulu5.jpg'];
+images.forEach(function(item,index){
+   preloadImg(item);
+});
+
+function preloadImg(path){
+   let imgTag = document.createElement('img');
+   imgTag.src = path;
+}
+
+let i = 2;
+function re(){
+ document.getElementById('mainImg').src = images[i%5];
+ i += 1;
+ repeat();
+}
+
+function repeat(){
+  setTimeout(re,500);
+}
+re();
+
 $(function(){
   $('.toggle_btn').on('click',function(){
     $('#page-header').toggleClass('open')
